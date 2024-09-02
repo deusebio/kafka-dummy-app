4894b4fcf2752f4fe470063b41b2129121894963
deusebio <enrico.deusebio@canonical.com>
2024-09-02 15:45:28 +0200

    [DPE-5290] Basic skeleton and tests (#1)

---
+++ kafka_app/core.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""KafkaClient class library for basic client connections to a Kafka Charm cluster.
+
+`KafkaClient` provides an interface from which to make generic client connections
+to a running Kafka Charm cluster. This can be as an administrator, producer or consumer,
+provided that the necessary ACLs and user credentials have already been created.
+
+Example usage for charms using `KafkaClient`:
+
+For a produer application
+```python
+
+    client = KafkaClient(
+        servers=bootstrap_servers,
+        username=username,
+        password=password,
+        security_protocol="SASL_PLAINTEXT",  # SASL_SSL for TLS enabled Kafka clusters
+    )
+
+    # if topic has not yet been created
+    if "admin" in roles:
+        topic_config = NewTopic(
+            name=topic,
+            num_partitions=5,
+            replication_factor=len(num_brokers)-1
+        )
+
+        logger.info(f"Creating new topic - {topic}")
+        client.create_topic(topic=topic_config)
+
+    logger.info("Producer - Starting...")
+    for message in SOME_ITERABLE:
+        client.produce_message(topic_name=topic, message_content=message)
+```
+
+Or, for a consumer application
+```python
+    client = KafkaClient(
+        servers=bootstrap_servers,
+        username=username,
+        password=password,
+        security_protocol="SASL_PLAINTEXT",  # SASL_SSL for TLS enabled Kafka clusters
+    )
+
+    logger.info("Consumer - Starting...")
+    client.subscribe_to_topic(topic_name=topic, consumer_group_prefix=consumer_group_prefix)
+    for message in client.messages():
+        logger.info(message)
+```
+"""
+from __future__ import annotations
+
+import logging
+from functools import cached_property
+from typing import Generator, List, Optional
+
+from kafka import KafkaAdminClient, KafkaConsumer, KafkaProducer
+from kafka.admin import NewTopic
+
+logger = logging.getLogger(__name__)
+
+class KafkaClient:
+    """Simplistic KafkaClient built on top of kafka-python."""
+
+    API_VERSION = (2, 5, 0)
+
+    def __init__(
+            self,
+            servers: List[str],
+            username: Optional[str],
+            password: Optional[str],
+            cafile_path: Optional[str] = None,
+            replication_factor: int = 3,
+    ) -> None:
+        self.servers = servers
+        self.username = username
+        self.password = password
+        self.cafile_path = cafile_path
+        self.replication_factor = replication_factor
+
+        self.security_protocol = "SASL_SSL" if cafile_path else "SASL_PLAINTEXT"
+
+        self._subscription = None
+        self._consumer_group_prefix = None
+
+    @cached_property
+    def _admin_client(self) -> KafkaAdminClient:
+        """Initialises and caches a `KafkaAdminClient`."""
+        return KafkaAdminClient(
+            client_id=self.username,
+            bootstrap_servers=self.servers,
+            ssl_check_hostname=False,
+            security_protocol=self.security_protocol,
+            sasl_plain_username=self.username,
+            sasl_plain_password=self.password,
+            sasl_mechanism="SCRAM-SHA-512",
+            ssl_cafile=self.cafile_path,
+        )
+
+    @cached_property
+    def _producer_client(self) -> KafkaProducer:
+        """Initialises and caches a `KafkaProducer`."""
+        return KafkaProducer(
+            bootstrap_servers=self.servers,
+            ssl_check_hostname=False,
+            security_protocol=self.security_protocol,
+            sasl_plain_username=self.username,
+            sasl_plain_password=self.password,
+            sasl_mechanism="SCRAM-SHA-512",
+            ssl_cafile=self.cafile_path,
+            acks="all",
+            retries=10,
+            retry_backoff_ms=1000,
+        )
+
+    @cached_property
+    def _consumer_client(self) -> KafkaConsumer:
+        """Initialises and caches a `KafkaConsumer`."""
+        return KafkaConsumer(
+            bootstrap_servers=self.servers,
+            ssl_check_hostname=False,
+            security_protocol=self.security_protocol,
+            sasl_plain_username=self.username,
+            sasl_plain_password=self.password,
+            sasl_mechanism="SCRAM-SHA-512",
+            ssl_cafile=self.cafile_path,
+            group_id=self._consumer_group_prefix,
+            enable_auto_commit=True,
+            auto_offset_reset="earliest",
+            consumer_timeout_ms=15000,
+        )
+
+    def create_topic(self, topic: NewTopic) -> None:
+        """Creates a new topic on the Kafka cluster.
+
+        Requires `KafkaClient` username to have `TOPIC CREATE` ACL permissions
+            for desired topic.
+
+        Args:
+            topic: the configuration of the topic to create
+        """
+        self._admin_client.create_topics(new_topics=[topic],
+                                         validate_only=False)
+
+    def delete_topics(self, topics: list[str]) -> None:
+        """Deletes a topic.
+
+        Args:
+            topics: list of topics to delete
+        """
+        self._admin_client.delete_topics(topics=topics)
+
+    def subscribe_to_topic(
+            self, topic_name: str, consumer_group_prefix: Optional[str] = None
+    ) -> None:
+        """Subscribes client to a specific topic, called when wishing to run a Consumer client.
+
+        Requires `KafkaClient` username to have `TOPIC READ` ACL permissions
+            for desired topic.
+        Optionally requires `KafkaClient` username to have `GROUP READ` ACL permissions
+            for desired consumer-group id.
+
+        Args:
+            topic_name: the topic to subscribe to
+            consumer_group_prefix: (optional) the consumer group_id prefix to join
+        """
+        self._consumer_group_prefix = (
+            consumer_group_prefix + "1" if consumer_group_prefix else None
+        )
+        self._subscription = topic_name
+        self._consumer_client.subscribe(topics=[topic_name])
+
+    def messages(self) -> Generator:
+        """Iterable of consumer messages.
+
+        Returns:
+            Generator of messages
+
+        Raises:
+            AttributeError: if topic not yet subscribed to
+        """
+        if not self._subscription:
+            msg = "Client not yet subscribed to a topic, cannot provide messages"
+            logger.error(msg)
+            raise AttributeError(msg)
+
+        yield from self._consumer_client
+
+    def produce_message(self, topic_name: str, message_content: bytes,
+                        timeout: int = 30) -> None:
+        """Sends message to target topic on the cluster.
+
+        Requires `KafkaClient` username to have `TOPIC WRITE` ACL permissions
+            for desired topic.
+
+        Args:
+            topic_name: the topic to send messages to
+            message_content: the content of the message to send
+            timeout: timeout for blocking after sending the message, defaults to 30s
+
+        Raises:
+            KafkaTimeoutError, KafkaError (general)
+        """
+        future = self._producer_client.send(topic_name, message_content)
+        future.get(timeout=timeout)
+        logger.debug(
+            f"Message published to topic={topic_name}, message content: {message_content}"
+        )
+
+    def close(self) -> None:
+        """Close the connection to the client."""
+        self._admin_client.close()
+        self._producer_client.close()
+        self._consumer_client.close()
+
+
---
+++ kafka_app/main.py
@@ -0,0 +1,137 @@
+import logging
+
+from kafka.admin import NewTopic
+import os
+
+import time
+
+import pandas as pd
+
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from kafka_app.core import KafkaClient
+import typer
+
+import uuid
+
+app = typer.Typer()
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+process_id = os.urandom(15).hex()
+
+@app.command()
+def create_topic(
+    topic_name: str,
+    num_partitions: int = 6, replication_factor: int = 3,
+    username: str = "user", password: str = "password",
+    bootstrap_server: str = "localhost:9092",
+    cafile_path: str | None = None,
+):
+    client = KafkaClient(
+        servers=bootstrap_server.split(","),
+        username=username,
+        password=password,
+        cafile_path=cafile_path
+    )
+
+    topic_config = NewTopic(
+        name=topic_name,
+        num_partitions=num_partitions, replication_factor=replication_factor
+    )
+
+    logger.info(f"Creating new topic - {topic_name}")
+
+    client.create_topic(topic=topic_config)
+
+
+@app.command()
+def producer(
+    topic_name: str,
+    num_messages: int,
+    username: str = "user", password: str = "password",
+    bootstrap_server: str = "localhost:9092",
+    cafile_path: str | None = None,
+):
+    client = KafkaClient(
+        servers=bootstrap_server.split(","),
+        username=username,
+        password=password,
+        cafile_path=cafile_path
+    )
+
+    logger.info("Producer - Starting...")
+    for ith in range(num_messages):
+        logger.info(f"Process {process_id} producing message with seq number {ith}")
+        message = f"{process_id}-{ith}"
+        client.produce_message(
+            topic_name=topic_name, message_content=message.encode("utf-8")
+        )
+        time.sleep(1.0)
+
+@app.command()
+def consumer(
+    topic_name: str, consumer_group: str = "cg",
+    username: str = "user", password: str = "password",
+    bootstrap_server: str = "localhost:9092",
+    cafile_path: str | None = None,
+    mongo_uri: str | None = None
+):
+    client = KafkaClient(
+        servers=bootstrap_server.split(","),
+        username=username,
+        password=password,
+        cafile_path=cafile_path
+    )
+
+    if mongo_uri:
+        mongo_client = MongoClient(mongo_uri)
+        collection = mongo_client[topic_name].get_collection("consumer")
+    else:
+        collection = None
+
+    logger.info("Consumer - Starting...")
+    client.subscribe_to_topic(topic_name=topic_name,
+                              consumer_group_prefix=consumer_group)
+
+    for message in client.messages():
+        logger.info(message)
+        content = message.value.decode("utf-8")
+        origin_id, sequence_number = content.split("-")
+        content = {"_id": uuid.uuid4().hex, "content": content,
+                   "origin_id": origin_id, "sequence_number": sequence_number,
+                   "destination": process_id, "consumer_group": consumer_group}
+        if collection is not None:
+            try:
+                collection.insert_one(content)
+            except DuplicateKeyError:
+                logger.error(f"Duplicated key with id: {content['_id']}")
+
+
+@app.command()
+def report(
+    topic_name: str, mongo_uri: str, fields: str = "destination",
+    output: str | None = None
+):
+    fields_lst = [field.strip() for field in fields.split(",")]
+
+    keys = {name: f"${name}" for name in fields_lst}
+
+    mongo_client = MongoClient(mongo_uri)
+    collection = mongo_client[topic_name].get_collection("consumer")
+
+    items = collection.aggregate([
+        {"$group": {"_id": keys, "count": {"$sum": 1}}}
+    ])
+
+    df = pd.DataFrame([
+        item["_id"] | {key: item[key] for key in item if key != "_id"}
+        for item in items
+    ]).set_index(fields_lst)
+
+    if output:
+        df.to_csv(output)
+
+if __name__ == "__main__":
+    app()
